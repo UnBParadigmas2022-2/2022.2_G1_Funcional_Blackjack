425e6bb7b19989aaa87bbb12819e92dc09431218
AlGouvea <alvarohsgouvea@gmail.com>
2022-11-24 02:06:25 -0300

    Adicionado perfil basico de usuario para armazenar quantidade de dinheiro de diferentes pessoas
    Co-authored-by: liversonp <liverson.p@gmail.com>
    Co-authored-by: francisco1code <francisco1code@gmail.com>

--- src/menu.hs
+++ src/menu.hs
@@ -7,38 +7,126 @@ import Data.Typeable
 import System.Exit
 import System.IO
 import System.Random
-
+import System.Directory
 
 main :: IO ()
 main = do
-  putStrLn "Bem-vindo ao BlackJack\n"
+  putStr "Bem-vindo ao BlackJack\nInforme seu nome: "
+  hFlush stdout
+  nome <- getLine
   handle <- openFile "game.txt" ReadWriteMode
-  string <- hGetContents handle
-  let money = read string::Int
-  mainMenu money
+  fileParser nome handle
 
-mainMenu :: Int -> IO ()
-mainMenu money = do
-  if money <= 0
-    then putStrLn "Você faliu...\n"
-    else do
-      putStrLn "Menu Principal do Jogo: "
-      putStrLn "1 - Iniciar partida"
-      putStrLn "2 - Salvar"
-      putStrLn "3 - Fechar\n"
+-- Atravessa o arquivo procurando o nome igual e confere a senha
+fileParser :: String -> Handle -> IO ()
+fileParser nome handle = do
+  isFileEnd <- hIsEOF handle
+  if isFileEnd 
+    then do
+      putStr "Nao foi encontrado um usuario com esse nome, deseja criar um novo usuario?\n1 - Sim\n2 - Nao\n"
       option <- getLine
       case option of
-        "1" -> startGameMenu money
-        "2" -> saveGame money
-        "3" -> putStrLn "O jogo esta sendo finalizado...\n"
+        "1" -> do
+            hClose handle
+            putStr "Defina sua senha: "
+            hFlush stdout
+            password <- getLine
+            appendFile "game.txt" (nome++"\n")
+            appendFile "game.txt" ("2000\n")
+            appendFile "game.txt" (password++"\n")
+            putStrLn ("Pronto! Seja Bem-Vindo "++nome++"!")
+            mainMenu nome 2000
+        "2" -> do
+            putStrLn "O sistema esta sendo finalizado"
+  else
+    do
+      fileName <- hGetLine handle
+      fileMoney <- hGetLine handle
+      filePass <- hGetLine handle
+      -- Trata a linha
+      if fileName == nome
+        then do
+          hClose handle
+          putStrLn ("Bem vindo de volta "++nome++"!")
+          putStr "Para continuar, informe sua senha: "
+          hFlush stdout
+          senha <- getLine
+          if senha == filePass
+            then do
+              mainMenu nome (read fileMoney::Int)
+          else do
+            putStrLn "Senha incorreta!\nFinalizando sistema!"
+        else do
+          fileParser nome handle
 
-saveGame :: Int -> IO ()
-saveGame money = do
-        writeFile "game.txt" (show money)
+      
+
+mainMenu :: String -> Int -> IO ()
+mainMenu nome money = do
+  if money <= 0
+    then putStrLn "Você faliu...\n"
+  else do
+    putStrLn "Menu Principal do Jogo: "
+    putStrLn "1 - Iniciar partida"
+    putStrLn "2 - Salvar"
+    putStrLn "3 - Fechar\n"
+    option <- getLine
+    case option of
+      "1" -> startGameMenu nome money
+      "2" -> do
+        handle <- openFile "game.txt" ReadWriteMode
+        saveGame nome money 0 [] handle
+        hClose handle
+      "3" -> putStrLn "O jogo esta sendo finalizado...\n"
+
+-- Funcao de append de lista tirada do site https://www.educative.io/answers/how-to-append-an-element-to-a-haskell-list
+-- Funcao alterada de Int para String
+append :: String -> [String] -> [String]
+append a xs = xs ++ [a]
+
+saveGame :: String-> Int -> Int -> [String] -> Handle -> IO ()
+saveGame nome money counter list handle = do
+  isFileEnd <- hIsEOF handle
+  if isFileEnd && (counter == 0)
+    then do
+      putStrLn "O sistema foi reiniciado e seu usuario apagado, por favor recrie."
+  else if isFileEnd && (counter /= 0) 
+    then do
+      hClose handle
+      removeFile "game.txt"
+      handle <- openFile "game.txt" ReadWriteMode
+      hClose handle
+      listPrintIterate list 0
+  else do
+    nameLine <- hGetLine handle
+    if (nameLine == nome)
+      then do
+        moneyLine <- hGetLine handle
+        passwordLine <- hGetLine handle
+        let nameList = append nome list
+        let moneyList = append (show money) nameList
+        let newList = append passwordLine moneyList
+        saveGame nome money (counter+1) newList handle
+    else do
+      moneyLine <- hGetLine handle
+      passwordLine <- hGetLine handle
+      let nameList = append nameLine list
+      let moneyList = append moneyLine nameList
+      let newList = append passwordLine moneyList
+      saveGame nome money (counter+1) newList handle
+
+listPrintIterate :: [String] -> Int-> IO ()
+listPrintIterate list index = do
+  if index < length list
+    then do
+      appendFile "game.txt" ((list !! index)++"\n")
+      listPrintIterate list (index+1)
+  else
+    putStrLn "O jogo esta sendo finalizado...\n"
 
 
-startGameMenu :: Int -> IO ()
-startGameMenu money = do
+startGameMenu :: String -> Int -> IO ()
+startGameMenu nome money = do
   putStrLn $ "Seu dinheiro: $ " ++ show money
   putStrLn "\nEscolha sua acao: "
   putStrLn "1 - Apostar 10"
@@ -60,54 +148,54 @@ startGameMenu money = do
       if (money - 10) < 0
         then do
           putStrLn "Dinheiro insuficiente para a aposta minima. Voltando para o menu principal..." 
-          mainMenu money
+          mainMenu nome money
       else do
         putStrLn $ "Apostando: " ++ show 10
-        inGameMenu 10 (money -10) playerHand dealerHand _deckShuffled
+        inGameMenu nome 10 (money -10) playerHand dealerHand _deckShuffled
     "2" -> do
       if (money - 50) < 0
         then do
           putStrLn "Dinheiro insuficiente!" 
-          startGameMenu money
+          startGameMenu nome money
       else do
         putStrLn $ "Apostando: " ++ show 50
-        inGameMenu 50 (money - 50) playerHand dealerHand _deckShuffled
+        inGameMenu nome 50 (money - 50) playerHand dealerHand _deckShuffled
     "3" -> do
       if (money - 100) < 0
         then do
           putStrLn "Dinheiro insuficiente!" 
-          startGameMenu money
+          startGameMenu nome money
       else do
         putStrLn $ "Apostando: " ++ show 100
-        inGameMenu 100 (money - 100) playerHand dealerHand _deckShuffled
+        inGameMenu nome 100 (money - 100) playerHand dealerHand _deckShuffled
     "4" -> do
       if (money - 250) < 0
         then do
           putStrLn "Dinheiro insuficiente!" 
-          startGameMenu money
+          startGameMenu nome money
       else do
         putStrLn $ "Apostando: " ++ show 250
-        inGameMenu 250 (money - 250) playerHand dealerHand _deckShuffled
+        inGameMenu nome 250 (money - 250) playerHand dealerHand _deckShuffled
     "5" -> do
       if (money - 500) < 0
         then do
           putStrLn "Dinheiro insuficiente!" 
-          startGameMenu money
+          startGameMenu nome money
       else do
         putStrLn $ "Apostando: " ++ show 500
-        inGameMenu 500 (money - 500) playerHand dealerHand _deckShuffled
+        inGameMenu nome 500 (money - 500) playerHand dealerHand _deckShuffled
     "6" -> do
         putStrLn "Voltando para o menu...\n"
-        mainMenu money
-    _ ->startGameMenu money
+        mainMenu nome money
+    _ ->startGameMenu nome money
 
 
-inGameMenu :: Int -> Int -> [([Char], Char)] -> [([Char], Char)] -> [([Char], Char)] -> IO ()
-inGameMenu bet totalMoney playerHand dealerHand deckShuffled = do
+inGameMenu :: String -> Int -> Int -> [([Char], Char)] -> [([Char], Char)] -> [([Char], Char)] -> IO ()
+inGameMenu nome bet totalMoney playerHand dealerHand deckShuffled = do
   putStrLn $ "\nSua mão:\n" ++ (printHand playerHand)
   putStrLn $ "Mão do dealer:\n" ++ (printHand dealerHand)
 
-  compareHandValuesOverOrEqual21 playerHand dealerHand bet totalMoney
+  compareHandValuesOverOrEqual21 playerHand dealerHand bet totalMoney nome
   putStrLn $ "Seu dinheiro: $ " ++ show totalMoney
   putStrLn $ "\n----------------------------------------\n" ++ "Escolha sua acao: "
   putStrLn "1 - Dobrar aposta"
@@ -119,11 +207,11 @@ inGameMenu bet totalMoney playerHand dealerHand deckShuffled = do
       if (totalMoney - bet) < 0
         then do
           putStrLn "Dinheiro insuficiente para dobrar a aposta"
-          inGameMenu bet totalMoney playerHand dealerHand deckShuffled
+          inGameMenu nome bet totalMoney playerHand dealerHand deckShuffled
         else do
           putStrLn $ "Dobrando aposta... Valor atual: " ++ show (bet * 2)
           let _playerHand = head deckShuffled : playerHand
-          inGameMenu (bet * 2) (totalMoney - bet) _playerHand dealerHand (drop 1 deckShuffled)
+          inGameMenu nome (bet * 2) (totalMoney - bet) _playerHand dealerHand (drop 1 deckShuffled)
     "2" -> do
       let _playerHand = head deckShuffled : playerHand
 
@@ -132,9 +220,9 @@ inGameMenu bet totalMoney playerHand dealerHand deckShuffled = do
       if handValueDealer < handValuePlayer
         then do
           let _dealerHand = head (drop 1 deckShuffled) : dealerHand
-          inGameMenu bet totalMoney _playerHand _dealerHand (drop 2 deckShuffled)
+          inGameMenu nome bet totalMoney _playerHand _dealerHand (drop 2 deckShuffled)
         else do
-          inGameMenu bet totalMoney _playerHand dealerHand (drop 1 deckShuffled)
+          inGameMenu nome bet totalMoney _playerHand dealerHand (drop 1 deckShuffled)
 
     "3" -> do
       putStrLn "Fechando mao...\n"
@@ -144,16 +232,16 @@ inGameMenu bet totalMoney playerHand dealerHand deckShuffled = do
       if handValueDealer < handValuePlayer
         then do
           let _dealerHand = head deckShuffled : dealerHand
-          compareHandValuesOverOrEqual21 playerHand _dealerHand bet totalMoney
-          compareHandValues playerHand _dealerHand bet totalMoney
-          mainMenu totalMoney
+          compareHandValuesOverOrEqual21 playerHand _dealerHand bet totalMoney nome
+          compareHandValues playerHand _dealerHand bet totalMoney nome
+          mainMenu nome totalMoney
         else do
-          compareHandValuesOverOrEqual21 playerHand dealerHand bet totalMoney
-          compareHandValues playerHand dealerHand bet totalMoney
-          mainMenu totalMoney
+          compareHandValuesOverOrEqual21 playerHand dealerHand bet totalMoney nome
+          compareHandValues playerHand dealerHand bet totalMoney nome
+          mainMenu nome totalMoney
 
-compareHandValuesOverOrEqual21 :: [([Char], Char)] -> [([Char], Char)] -> Int -> Int -> IO ()
-compareHandValuesOverOrEqual21 hand dealerHand bet totalMoney = do
+compareHandValuesOverOrEqual21 :: [([Char], Char)] -> [([Char], Char)] -> Int -> Int -> String -> IO ()
+compareHandValuesOverOrEqual21 hand dealerHand bet totalMoney nome = do
   let handValue = getHandValue hand
       handValueDealer = getHandValue dealerHand
   putStrLn $ "Valor da sua mão " ++ show handValue
@@ -161,38 +249,38 @@ compareHandValuesOverOrEqual21 hand dealerHand bet totalMoney = do
   if handValue > 21 || handValueDealer == 21
     then do
       putStrLn "Dealer venceu\n"
-      mainMenu totalMoney
+      mainMenu nome totalMoney
       exitSuccess
     else
       if handValueDealer > 21 || handValue == 21
         then do
           putStrLn "Você venceu\n"
           putStrLn $ "Seu dinheiro: $ " ++ show (totalMoney + (2 * bet)) ++ "\n"
-          mainMenu (totalMoney + (2 * bet))
+          mainMenu nome (totalMoney + (2 * bet))
           exitSuccess
         else do
           putStrLn ""
 
-compareHandValues :: [([Char], Char)] -> [([Char], Char)] -> Int -> Int -> IO ()
-compareHandValues hand dealerHand bet totalMoney = do
+compareHandValues :: [([Char], Char)] -> [([Char], Char)] -> Int -> Int -> String -> IO ()
+compareHandValues hand dealerHand bet totalMoney nome = do
   let handValue = getHandValue hand
       handValueDealer = getHandValue dealerHand
   if handValueDealer > handValue
     then do
       putStrLn "Dealer venceu\n"
-      mainMenu totalMoney
+      mainMenu nome totalMoney
       exitSuccess
     else
       if handValue > handValueDealer
         then do
           putStrLn "Você venceu\n"
           putStrLn $ "Seu dinheiro: $ " ++ show (totalMoney + (2 * bet)) ++ "\n"
-          mainMenu (totalMoney + (2 * bet))
+          mainMenu nome (totalMoney + (2 * bet))
           exitSuccess
         else do
           putStrLn "Empate\n"
           putStrLn $ "Seu dinheiro: $ " ++ show (totalMoney + bet) ++ "\n"
-          mainMenu (totalMoney + bet)
+          mainMenu nome (totalMoney + bet)
           exitSuccess
 
 getHandValue :: [(a, Char)] -> Int
